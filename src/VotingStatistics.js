19acc195615fcc9581c446e340a9f81c51dec4a2
Christian Blume <chris.blume@gmx.net>
2017-10-08 22:50:14 +0200

    Added comments.

--- src/VotingStatistics.js
+++ src/VotingStatistics.js
@@ -8,6 +8,11 @@ const VotingStatistics = (props) => {
     let upVoteChart;
     let downVoteChart;
 
+    /*
+		If there are no up- or down-votes no chart displaying corresponding data should be rendered.
+		Unfortunately, if no data is available there will still be a heading for the invisible chart.
+		That's what this workaround is about.
+    */
     if (props.totalUpVotes > 0) {
         upVoteChart = 
             <Chart
