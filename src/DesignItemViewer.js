f4a685b915b5dabbd8b695b12e44ffd75a4c535c
Christian Blume <chris.blume@gmx.net>
2017-10-08 22:41:57 +0200

    Updated DesignItemViewer component to a stateless component. Added comments.

--- src/DesignItemViewer.js
+++ src/DesignItemViewer.js
@@ -2,43 +2,47 @@ import React from 'react';
 
 import DesignItem from './DesignItem';
 
-class DesignItemViewer extends React.Component {
-    render() {
-        let rightButton;
-        if (this.props.loadMoreDesigns) {
-            rightButton = <i className='large plus icon' onClick={this.props.onLoadMoreDesigns} />;
-        } else {
-            rightButton = <i className='large chevron right icon' onClick={this.props.onSkipForward} />;
-        }
+const DesignItemViewer = (props) => {
 
-        return (
-            <div className='row'>
-                <div className='sixteen wide mobile twelve wide computer column'>
-                    <div className='ui three column centered grid'>
-                        <div className='three wide mobile one wide computer middle aligned column'>
-                            <i 
-                                className={(this.props.disableBackward ? 'disabled ' : '') + 'large chevron left icon'} 
-                                onClick={this.props.onSkipBackward}
-                            />
-                        </div>
-                        <div className='ten wide mobile seven wide computer column'>
-                            <DesignItem
-                                src={this.props.design.src}
-                                name={this.props.design.name}
-                                rating={this.props.design.rating}
-                                keyword={this.props.keyword}
-                                onUpVote={this.props.onUpVote}
-                                onDownVote={this.props.onDownVote}
-                            />
-                        </div>
-                        <div className='three wide mobile one wide computer middle aligned column'>
-                            {rightButton}
-                        </div>
+	/*
+		If `props.loadMoreDesigns === true` a plus sign rather than a right pointing chevron will be rendered
+		on the right side of the displayed design, indicating the ability to load more designs (matching the 
+		same keyword).
+	*/
+    let rightButton;
+    if (props.loadMoreDesigns) {
+        rightButton = <i className='large plus icon' onClick={props.onLoadMoreDesigns} />;
+    } else {
+        rightButton = <i className='large chevron right icon' onClick={props.onSkipForward} />;
+    }
+
+    return (
+        <div className='row'>
+            <div className='sixteen wide mobile twelve wide computer column'>
+                <div className='ui three column centered grid'>
+                    <div className='three wide mobile one wide computer middle aligned column'>
+                        <i 
+                            className={(props.disableBackward ? 'disabled ' : '') + 'large chevron left icon'} 
+                            onClick={props.onSkipBackward}
+                        />
+                    </div>
+                    <div className='ten wide mobile seven wide computer column'>
+                        <DesignItem
+                            src={props.design.src}
+                            name={props.design.name}
+                            rating={props.design.rating}
+                            keyword={props.keyword}
+                            onUpVote={props.onUpVote}
+                            onDownVote={props.onDownVote}
+                        />
+                    </div>
+                    <div className='three wide mobile one wide computer middle aligned column'>
+                        {rightButton}
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default DesignItemViewer;
